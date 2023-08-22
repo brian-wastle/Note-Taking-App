093eead05ea157620ce66a752e38211ae2e984f4
brian-wastle <brian.wastle1@gmail.com>
2023-08-22 10:00:26 -0400

    added POST request

--- server.js
+++ server.js
@@ -6,3 +6,13 @@ const repos = require("./db/repos.json")
 
 
 app.use(express.static('public'));
+
+app.listen(4000);
+
+app.get('/notes', (req, res) =>
+  res.sendFile(path.join(__dirname, 'public/notes.html'))
+);
+
+app.post('/notes', (req, res) =>
+  {console.log(req.body);}
+);
