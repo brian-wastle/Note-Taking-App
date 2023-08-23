109231dba616b8253b5503e4aa0789e36e2e692c
brian-wastle <brian.wastle1@gmail.com>
2023-08-23 09:09:48 -0400

    fixed get request routes

--- server.js
+++ server.js
@@ -13,44 +13,52 @@ app.use(express.static('public'));
 
 // app.use('/api', api);
 
-app.get('/notes', (req, res) =>
-  res.sendFile(path.join(__dirname, 'public/notes.html'))
+// app.get('/*', (req, res) => {
+//   res.sendFile(path.join(__dirname, './public/index.html'));
+// });
+
+app.get('/notes', (req, res) => 
+  res.sendFile(path.join(__dirname, './public/notes.html')),
 );
 
-app.post('/notes', (req, res) => {
-    
-    const { title, text } = req.body;
 
-    if (title && text) {
-        // Variable for the object we will save
-        const note = {
-            title,
-            text,
-          };
-        repos.push(note);
-    
-        // Convert the data to a string so we can save it
-        const notesStrings = JSON.stringify(repos, null, 2);
-    
-        // Write the string to a file
-        fs.writeFile(`./db/repos.json`, notesStrings, (err) =>
-          err
-            ? console.error(err)
-            : console.log(
-                `Note named "${note.title}" has been written to JSON file`
-              )
-        );
 
-        res.status(201).json(res);
-        
+app.get('/api/notes', (req, res) => {
+  fs.readFile('./db/repos.json', 'utf8', (err, data) => {
+    if (err) {
+      console.error(err);
+      return;
+    }
+    res.send(data);
+  });  
+});
+
+
+app.post('/api/notes', (req, res) => {
+    
+  const { title, text } = req.body;
+
+  if (title && text) {
+    // Variable for the object we will save
+    const note = {
+        title,
+        text,
+      };
+    repos.push(note);
+
+    // Convert the data to a string so we can save it
+    const notesStrings = JSON.stringify(repos, null, 2);
+
+    // Write the string to a file
+    fs.writeFile(`./db/repos.json`, notesStrings, (err) => {
+      if (err) {
+        console.error(err);
+        res.status(500).json('Error in writing note to JSON file');
       } else {
-        res.status(500).json('Error in posting review');
+        res.status(201).json('Note added successfully');
       }
-
-
-
-}
-  
-);
+    })
+  }
+});
 
 app.listen(4000);
